9f379ca027cc7506fc613f30e9c478915331b87c
muhiza <muhizafrank@gmail.com>
2016-09-03 11:46:11 +0200

    Accessing static files

--- ecommerce2/wsgi.py
+++ ecommerce2/wsgi.py
@@ -7,8 +7,16 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
+import os
+
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ecommerce2.settings")
 
 application = get_wsgi_application()
+
+from whitenoise.django import DjangoWhiteNoise
 application = DjangoWhiteNoise(application)
+
+
+
